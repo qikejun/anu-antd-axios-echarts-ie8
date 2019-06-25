cabc57e648f5bcc17e420609117d0db11767067d
Dan <1471947564@qq.com>
2019-06-25 16:46:39 +0800

    调整

--- src/assets/ie8.js
+++ src/assets/ie8.js
@@ -135,3 +135,20 @@ if (!document.querySelector) {
         return (elements.length) ? elements[0] : null;
     };
 }
+// Object.create
+if (typeof Object.create !== "function") {
+    Object.create = function (proto, propertiesObject) {
+        if (typeof proto !== 'object' && typeof proto !== 'function') {
+            throw new TypeError('Object prototype may only be an Object: ' + proto);
+        } else if (proto === null) {
+            throw new Error("This browser's implementation of Object.create is a shim and doesn't support 'null' as the first argument.");
+        }
+
+        if (typeof propertiesObject != 'undefined') throw new Error("This browser's implementation of Object.create is a shim and doesn't support a second argument.");
+
+        function F() {}
+        F.prototype = proto;
+
+        return new F();
+    };
+}
